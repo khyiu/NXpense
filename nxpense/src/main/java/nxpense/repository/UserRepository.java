cbbfede22ada69d648429b4577f0d2b3d1358f72
Kar Hoo Yiu <karhooyiu@gmail.com>
2014-12-26 16:01:04 +0100

    Adding Spring Data JPA and repository structure

---
+++ nxpense/src/main/java/nxpense/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package nxpense.repository;
+
+import nxpense.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	public User findByEmail(String email);
+}
