ad90c7d71131e09c6aeaccd891f2d12e739787e3
Kar Hoo Yiu <superk888@gmail.com>
2014-12-28 00:30:37 +0100

    Update registration process

---
+++ nxpense/src/main/java/nxpense/exeption/RequestCannotCompleteException.java
@@ -0,0 +1,14 @@
+package nxpense.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class RequestCannotCompleteException extends RuntimeException{
+
+    private static final long serialVersionUID = 7209170909770768104L;
+
+    public RequestCannotCompleteException(String cause) {
+	super(cause);
+    }
+}
--- nxpense/src/main/java/nxpense/service/UserServiceImpl.java
+++ nxpense/src/main/java/nxpense/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import nxpense.domain.User;
 import nxpense.domain.UserAccount;
+import nxpense.exeption.RequestCannotCompleteException;
 import nxpense.repository.UserRepository;
 import nxpense.service.api.UserService;
 
@@ -16,24 +17,31 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl implements UserService{
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	public void createUser(String email, char[] password, char[] passwordRepeat) {
-		LOGGER.info("Creating new user with email={}", email);
-		
-		User user = new User();
-		user.setEmail(email);
-		user.setPassword(password);
-		
-		UserAccount userAccount = new UserAccount();
-		userAccount.setVerifiedCapital(BigDecimal.ZERO);
-		userAccount.setVerifiedDate(new Date());
-		user.setUserAccount(userAccount);
-		
-		userRepository.save(user);		
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUser(String email, char[] password, char[] passwordRepeat) {
+	LOGGER.info("Creating new user with email={}", email);
+
+	User existingUser = userRepository.findByEmail(email);
+
+	if(existingUser != null) {
+	    LOGGER.error("The provided email ({}) is already taken!", email);
+	    throw new RequestCannotCompleteException("This email is already taken");
 	}
+
+	User user = new User();
+	user.setEmail(email);
+	user.setPassword(password);
+
+	UserAccount userAccount = new UserAccount();
+	userAccount.setVerifiedCapital(BigDecimal.ZERO);
+	userAccount.setVerifiedDate(new Date());
+	user.setUserAccount(userAccount);
+
+	userRepository.save(user);		
+    }
 
 }
