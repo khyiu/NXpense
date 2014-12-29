8aeca3495dd2e08875cdcf951ae90a64e976bde0
Kar Hoo Yiu <superk888@gmail.com>
2014-12-29 11:15:28 +0100

    Indented code

--- nxpense/src/main/java/nxpense/exeption/RequestCannotCompleteException.java
+++ nxpense/src/main/java/nxpense/exeption/RequestCannotCompleteException.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.CONFLICT)
 public class RequestCannotCompleteException extends RuntimeException{
 
-    private static final long serialVersionUID = 7209170909770768104L;
+	private static final long serialVersionUID = 7209170909770768104L;
 
-    public RequestCannotCompleteException(String cause) {
-	super(cause);
-    }
+	public RequestCannotCompleteException(String cause) {
+		super(cause);
+	}
 }
