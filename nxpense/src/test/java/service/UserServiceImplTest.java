19973ea80b66a511dbdff609d7650f2eefb621c9
Kar Hoo Yiu <superk888@gmail.com>
2014-12-31 00:04:49 +0100

    Start adding unit tests + Mockito dependencies

--- nxpense/src/test/java/service/UserServiceImplTest.java
+++ nxpense/src/test/java/service/UserServiceImplTest.java
@@ -1,6 +1,46 @@
 package service;
 
+import static org.mockito.BDDMockito.*;
+import nxpense.domain.User;
+import nxpense.repository.UserRepository;
+import nxpense.service.UserServiceImpl;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
-    
+
+	private static final String EMAIL_NEW = "new@test.com";
+	private static final String PASSWORD = "gue$$it";
+
+	@InjectMocks
+	private UserServiceImpl userService;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private UserDetailsService userDetailsService;
+
+	@Mock
+	private PasswordEncoder passwordEncoder;
+	
+	@Before
+	public void configureMocks() {
+		given(passwordEncoder.encode(any(CharSequence.class))).willReturn(PASSWORD);
+	}
+
+
+	@Test
+	public void testCreateUser() {	 
+		userService.createUser(EMAIL_NEW, PASSWORD.toCharArray(), PASSWORD.toCharArray());
+		verify(userRepository, times(1)).save(any(User.class));
+	}
 }
