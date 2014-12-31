db87629fe9a522ac3c18c4a294e725961fd531ef
Kar Hoo Yiu <superk888@gmail.com>
2014-12-31 18:06:42 +0100

    Indent code

--- nxpense/src/main/java/nxpense/security/CustomUserDetails.java
+++ nxpense/src/main/java/nxpense/security/CustomUserDetails.java
@@ -7,43 +7,43 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetails implements UserDetails {
 
-	private static final long serialVersionUID = -3060572330933644236L;
+    private static final long serialVersionUID = -3060572330933644236L;
 
-	private String email;
-	private String password;
-	
-	public CustomUserDetails(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}
-	
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private String email;
+    private String password;
 
-	public String getPassword() {
-		return this.password;
-	}
+    public CustomUserDetails(String email, String password) {
+	this.email = email;
+	this.password = password;
+    }
 
-	public String getUsername() {
-		return this.email;
-	}
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+	// TODO Auto-generated method stub
+	return null;
+    }
 
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    public String getPassword() {
+	return this.password;
+    }
 
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    public String getUsername() {
+	return this.email;
+    }
 
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    public boolean isAccountNonExpired() {
+	return true;
+    }
 
-	public boolean isEnabled() {
-		return true;
-	}
+    public boolean isAccountNonLocked() {
+	return true;
+    }
+
+    public boolean isCredentialsNonExpired() {
+	return true;
+    }
+
+    public boolean isEnabled() {
+	return true;
+    }
 
 }
