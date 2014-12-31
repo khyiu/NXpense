714ad071288315e305b856ef3f2714f001f067bc
Kar Hoo Yiu <superk888@gmail.com>
2014-12-31 17:48:38 +0100

    Indent code

--- nxpense/src/main/java/nxpense/service/UserServiceImpl.java
+++ nxpense/src/main/java/nxpense/service/UserServiceImpl.java
@@ -27,62 +27,62 @@ import org.springframework.stereotype.Service;
 @Service("userService")
 public class UserServiceImpl implements UserService{
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
 
-	@Autowired
-	@Qualifier("bcryptEncoder")
-	private PasswordEncoder passwordEncoder;
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
+    @Autowired
+    @Qualifier("bcryptEncoder")
+    private PasswordEncoder passwordEncoder;
 
-	public void createUser(String email, char[] password, char[] passwordRepeat) {
-		LOGGER.info("Creating new user with email={}", email);
+    @Autowired
+    private UserRepository userRepository;
 
-		checkPasswordsMatch(password, passwordRepeat);
-		checkEmailAlreadyTaken(email);
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-		User newUser = initializeNewUserWithAccount(email, password);
-		userRepository.save(newUser);
+    public void createUser(String email, char[] password, char[] passwordRepeat) {
+	LOGGER.info("Creating new user with email={}", email);
 
-		// Programmatically authenticate the user that just registered so that he/she does not have to 
-		// go through the login process all over again.
-		// NOTE: it is important to use the constructor with a List<GrantedAuthority> as it marks 
-		// the Authentication object as authenticated.
-		UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, password, Collections.<GrantedAuthority>emptyList());
-		SecurityContextHolder.getContext().setAuthentication(authentication);
+	checkPasswordsMatch(password, passwordRepeat);
+	checkEmailAlreadyTaken(email);
+
+	User newUser = initializeNewUserWithAccount(email, password);
+	userRepository.save(newUser);
+
+	// Programmatically authenticate the user that just registered so that he/she does not have to 
+	// go through the login process all over again.
+	// NOTE: it is important to use the constructor with a List<GrantedAuthority> as it marks 
+	// the Authentication object as authenticated.
+	UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+	Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, password, Collections.<GrantedAuthority>emptyList());
+	SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void checkEmailAlreadyTaken(String email) {
+	User existingUser = userRepository.findByEmail(email);
+
+	if(existingUser != null) {
+	    LOGGER.error("The provided email ({}) is already taken!", email);
+	    throw new RequestCannotCompleteException("This email is already taken");
 	}
+    }
 
-	private void checkEmailAlreadyTaken(String email) {
-		User existingUser = userRepository.findByEmail(email);
-
-		if(existingUser != null) {
-			LOGGER.error("The provided email ({}) is already taken!", email);
-			throw new RequestCannotCompleteException("This email is already taken");
-		}
+    private void checkPasswordsMatch(char [] password, char [] passwordRepeat) {
+	if(!Arrays.equals(password, passwordRepeat)) {
+	    LOGGER.error("Password confirmation does not match password!");
+	    throw new RequestCannotCompleteException("Password and password confirmation don't match");
 	}
+    }
 
-	private void checkPasswordsMatch(char [] password, char [] passwordRepeat) {
-		if(!Arrays.equals(password, passwordRepeat)) {
-			LOGGER.error("Password confirmation does not match password!");
-			throw new RequestCannotCompleteException("Password and password confirmation don't match");
-		}
-	}
+    private User initializeNewUserWithAccount(String email, char [] password) {
+	char [] encodedPassword = passwordEncoder.encode(java.nio.CharBuffer.wrap(password)).toCharArray();
+	User user = new User();
+	user.setEmail(email);
+	user.setPassword(encodedPassword);
 
-	private User initializeNewUserWithAccount(String email, char [] password) {
-		char [] encodedPassword = passwordEncoder.encode(java.nio.CharBuffer.wrap(password)).toCharArray();
-		User user = new User();
-		user.setEmail(email);
-		user.setPassword(encodedPassword);
-
-		UserAccount userAccount = new UserAccount();
-		userAccount.setVerifiedCapital(BigDecimal.ZERO);
-		userAccount.setVerifiedDate(new Date());
-		user.setUserAccount(userAccount);
-		return user;
-	}
+	UserAccount userAccount = new UserAccount();
+	userAccount.setVerifiedCapital(BigDecimal.ZERO);
+	userAccount.setVerifiedDate(new Date());
+	user.setUserAccount(userAccount);
+	return user;
+    }
 }
