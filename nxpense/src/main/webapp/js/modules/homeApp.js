bd8a8ba199255e178cf006ddf2d8eb58ffca2c2f
Kar Hoo Yiu <superk888@gmail.com>
2015-02-14 21:46:52 +0100

    Update modal proto: prevent focus from going outside the popup when 'tabbing'

--- nxpense/src/main/webapp/js/modules/homeApp.js
+++ nxpense/src/main/webapp/js/modules/homeApp.js
@@ -1,44 +1,59 @@
 (function(angular) {
-  'use strict';
+	'use strict';
 
-  var homeAppModule = angular.module('homeApp', ['ui.bootstrap']);
+	var homeAppModule = angular.module('homeApp', ['ui.bootstrap']);
 
-  homeAppModule.controller('userController', ['$scope', function($scope) {
-    $scope.logout = function() {
-      window.location.assign('/nxpense/logout');
-    };
-  }]);
+	homeAppModule.controller('userController', ['$scope', function($scope) {
+		$scope.logout = function() {
+			window.location.assign('/nxpense/logout');
+		};
+	}]);
 
-  homeAppModule.controller('expenseController', ['$scope', '$modal', function($scope, $modal) {
-    $scope.openNewExpenseModal = function() {
-      var modalInstance = $modal.open({
-        templateUrl: 'modal/new-expense-modal.html',
-        controller: 'modalController',
-        size: 'lg'
-      });
-    }
-  }]);
+	homeAppModule.controller('expenseController', ['$scope', '$modal', function($scope, $modal) {
+		$scope.openNewExpenseModal = function() {
+			var modalInstance = $modal.open({
+				templateUrl: 'modal/new-expense-modal.html',
+				controller: 'modalController',
+				size: 'lg'
+			});
+		}
+	}]);
 
-  homeAppModule.controller('modalController', ['$scope', '$modalInstance', function($scope, $modalInstance){
-    $scope.opened = false;
+	homeAppModule.controller('modalController', ['$scope', '$modalInstance', function($scope, $modalInstance){
+		$scope.opened = false;
 
-    $scope.ok = function() {
-      console.log('>>> clicked on modal OK');
-      $modalInstance.close();
-    };
+		$scope.ok = function() {
+			$modalInstance.close();
+		};
 
-    $scope.cancel = function() {
-      console.log('>>> clicked on modal CANCEL');
-      $modalInstance.dismiss('cancel');
-    };
+		$scope.cancel = function() {
+			console.log('>>> clicked on modal CANCEL');
+		};
 
-    $scope.openCal = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
-      console.log('>>> open cal');
+		$scope.openCal = function($event) {
+			$event.preventDefault();
+			$event.stopPropagation();
 
-      $scope.opened = true;
-    };
-  }]);
+			$scope.opened = true;
+		};
+
+		$scope.processKey = function($event) {
+			var tabbed = $event.keyCode === 9 && !$event.shiftKey;
+			var backTabbed = $event.keyCode === 9 && $event.shiftKey;
+			var activeElement = $(document.activeElement)[0];
+			var firstTabbableElement;
+			var lastTabbableElement;
+			
+			if(tabbed && activeElement.hasAttribute('lastTab')) {
+				$event.preventDefault();
+				firstTabbableElement = $(document).find('*[firstTab]');
+				firstTabbableElement.focus();
+			} else if(backTabbed && activeElement.hasAttribute('firstTab')) {
+				$event.preventDefault();
+				lastTabbableElement = $(document).find('*[lastTab]');
+				lastTabbableElement.focus();
+			}
+		};
+	}]);
 
 })(window.angular);
