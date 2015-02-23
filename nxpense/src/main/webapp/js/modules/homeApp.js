403a0e0f16111261b8a8ddfa1e05f28d5028c954
khyiu <superk888@gmail.com>
2015-02-23 14:38:11 +0100

    Code indentation

--- nxpense/src/main/webapp/js/modules/homeApp.js
+++ nxpense/src/main/webapp/js/modules/homeApp.js
@@ -1,60 +1,60 @@
 (function(angular) {
-	'use strict';
+  'use strict';
 
-	var homeAppModule = angular.module('homeApp', ['ui.bootstrap', 'restangular']);
+  var homeAppModule = angular.module('homeApp', ['ui.bootstrap', 'restangular']);
 
-	homeAppModule.controller('userController', ['$scope', function($scope) {
-		$scope.logout = function() {
-			window.location.assign('/nxpense/logout');
-		};
-	}]);
+  homeAppModule.controller('userController', ['$scope', function($scope) {
+    $scope.logout = function() {
+      window.location.assign('/nxpense/logout');
+    };
+  }]);
 
-	homeAppModule.controller('expenseController', ['$scope', '$modal', function($scope, $modal) {
-		$scope.openNewExpenseModal = function() {
-            $modal.open({
-				templateUrl: 'modal/new-expense-modal.html',
-				controller: 'modalController'				
-			});
-		}
-	}]);
+  homeAppModule.controller('expenseController', ['$scope', '$modal', function($scope, $modal) {
+    $scope.openNewExpenseModal = function() {
+      $modal.open({
+        templateUrl: 'modal/new-expense-modal.html',
+        controller: 'modalController'
+      });
+    }
+  }]);
 
-	homeAppModule.controller('modalController', ['$scope', '$modalInstance', 'Restangular', function($scope, $modalInstance, Restangular){
-        // todo make root route 'nxpense' somehow global --> using Factory?
-        var expenseDAO = Restangular.one('nxpense');
+  homeAppModule.controller('modalController', ['$scope', '$modalInstance', 'Restangular', function($scope, $modalInstance, Restangular) {
+    // todo make root route 'nxpense' somehow global --> using Factory?
+    var expenseDAO = Restangular.one('nxpense');
 
-		$scope.ok = function() {
-            alert('>>> todo: persist expense data');
-            expenseDAO.post('account', {});
-		};
+    $scope.ok = function() {
+      alert('>>> todo: persist expense data');
+      expenseDAO.post('account', {});
+    };
 
-		$scope.cancel = function() {
-            $modalInstance.close();
-		};
+    $scope.cancel = function() {
+      $modalInstance.close();
+    };
 
-		$scope.openCal = function($event) {
-			$event.preventDefault();
-			$event.stopPropagation();
+    $scope.openCal = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
 
-			$scope.calOpened = true;
-		};
+      $scope.calOpened = true;
+    };
 
-		$scope.processKey = function($event) {
-			var tabbed = $event.keyCode === 9 && !$event.shiftKey;
-			var backTabbed = $event.keyCode === 9 && $event.shiftKey;
-			var activeElement = $(document.activeElement)[0];
-			var firstTabbableElement;
-			var lastTabbableElement;
-			
-			if(tabbed && activeElement.hasAttribute('lastTab')) {
-				$event.preventDefault();
-				firstTabbableElement = $(document).find('*[firstTab]');
-				firstTabbableElement.focus();
-			} else if(backTabbed && activeElement.hasAttribute('firstTab')) {
-				$event.preventDefault();
-				lastTabbableElement = $(document).find('*[lastTab]');
-				lastTabbableElement.focus();
-			}
-		};
-	}]);
+    $scope.processKey = function($event) {
+      var tabbed = $event.keyCode === 9 && !$event.shiftKey;
+      var backTabbed = $event.keyCode === 9 && $event.shiftKey;
+      var activeElement = $(document.activeElement)[0];
+      var firstTabbableElement;
+      var lastTabbableElement;
+
+      if (tabbed && activeElement.hasAttribute('lastTab')) {
+        $event.preventDefault();
+        firstTabbableElement = $(document).find('*[firstTab]');
+        firstTabbableElement.focus();
+      } else if (backTabbed && activeElement.hasAttribute('firstTab')) {
+        $event.preventDefault();
+        lastTabbableElement = $(document).find('*[lastTab]');
+        lastTabbableElement.focus();
+      }
+    };
+  }]);
 
 })(window.angular);
