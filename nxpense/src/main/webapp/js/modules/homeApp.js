9a6e52cdfc9eccfbcc0cfd61a7e480c9268d60df
Kar Hoo Yiu <superk888@gmail.com>
2015-03-11 13:07:09 +0100

    Indent code

--- nxpense/src/main/webapp/js/modules/homeApp.js
+++ nxpense/src/main/webapp/js/modules/homeApp.js
@@ -1,67 +1,67 @@
-(function(angular) {
-  'use strict';
+(function (angular) {
+    'use strict';
 
-  var homeAppModule = angular.module('homeApp', ['ui.bootstrap', 'restangular']);
+    var homeAppModule = angular.module('homeApp', ['ui.bootstrap', 'restangular']);
 
-  homeAppModule.controller('userController', ['$scope', function($scope) {
-    $scope.logout = function() {
-      window.location.assign('/nxpense/logout');
-    };
-  }]);
+    homeAppModule.controller('userController', ['$scope', function ($scope) {
+        $scope.logout = function () {
+            window.location.assign('/nxpense/logout');
+        };
+    }]);
 
-  homeAppModule.controller('expenseController', ['$scope', '$modal', function($scope, $modal) {
-    $scope.openNewExpenseModal = function() {
-      $modal.open({
-        templateUrl: 'modal/new-expense-modal.html',
-        controller: 'modalController'
-      });
-    }
-  }]);
+    homeAppModule.controller('expenseController', ['$scope', '$modal', function ($scope, $modal) {
+        $scope.openNewExpenseModal = function () {
+            $modal.open({
+                templateUrl: 'modal/new-expense-modal.html',
+                controller: 'modalController'
+            });
+        }
+    }]);
 
-  homeAppModule.controller('modalController', ['$scope', '$modalInstance', 'Restangular', '$filter', function($scope, $modalInstance, Restangular, $filter) {
-    // todo make root route 'nxpense' somehow global --> using Factory?
-    var expenseDAO = Restangular.one('nxpense');
+    homeAppModule.controller('modalController', ['$scope', '$modalInstance', 'Restangular', '$filter', function ($scope, $modalInstance, Restangular, $filter) {
+        // todo make root route 'nxpense' somehow global --> using Factory?
+        var expenseDAO = Restangular.one('nxpense');
 
-    $scope.newExpense = {
-      source: 'DEBIT_CARD'
-    }
+        $scope.newExpense = {
+            source: 'DEBIT_CARD'
+        }
 
-    $scope.ok = function() {
-      // NOTE: 'date' filter is applied on the input date with a format that will strip down the time part.
-      //       As a result, we don't need to worry about timezone side-effects
-      this.newExpense.date =  $filter('date')($scope.newExpense.date, 'dd/MM/yyyy'),
-      console.log(this.newExpense);
-      expenseDAO.post('expense/new', this.newExpense);
-    };
+        $scope.ok = function () {
+            // NOTE: 'date' filter is applied on the input date with a format that will strip down the time part.
+            //       As a result, we don't need to worry about timezone side-effects
+            this.newExpense.date = $filter('date')($scope.newExpense.date, 'dd/MM/yyyy'),
+                console.log(this.newExpense);
+            expenseDAO.post('expense/new', this.newExpense);
+        };
 
-    $scope.cancel = function() {
-      $modalInstance.close();
-    };
+        $scope.cancel = function () {
+            $modalInstance.close();
+        };
 
-    $scope.openCal = function($event) {
-      $event.preventDefault();
-      $event.stopPropagation();
+        $scope.openCal = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
 
-      $scope.calOpened = true;
-    };
+            $scope.calOpened = true;
+        };
 
-    $scope.processKey = function($event) {
-      var tabbed = $event.keyCode === 9 && !$event.shiftKey;
-      var backTabbed = $event.keyCode === 9 && $event.shiftKey;
-      var activeElement = $(document.activeElement)[0];
-      var firstTabbableElement;
-      var lastTabbableElement;
+        $scope.processKey = function ($event) {
+            var tabbed = $event.keyCode === 9 && !$event.shiftKey;
+            var backTabbed = $event.keyCode === 9 && $event.shiftKey;
+            var activeElement = $(document.activeElement)[0];
+            var firstTabbableElement;
+            var lastTabbableElement;
 
-      if (tabbed && activeElement.hasAttribute('lastTab')) {
-        $event.preventDefault();
-        firstTabbableElement = $(document).find('*[firstTab]');
-        firstTabbableElement.focus();
-      } else if (backTabbed && activeElement.hasAttribute('firstTab')) {
-        $event.preventDefault();
-        lastTabbableElement = $(document).find('*[lastTab]');
-        lastTabbableElement.focus();
-      }
-    };
-  }]);
+            if (tabbed && activeElement.hasAttribute('lastTab')) {
+                $event.preventDefault();
+                firstTabbableElement = $(document).find('*[firstTab]');
+                firstTabbableElement.focus();
+            } else if (backTabbed && activeElement.hasAttribute('firstTab')) {
+                $event.preventDefault();
+                lastTabbableElement = $(document).find('*[lastTab]');
+                lastTabbableElement.focus();
+            }
+        };
+    }]);
 
 })(window.angular);
