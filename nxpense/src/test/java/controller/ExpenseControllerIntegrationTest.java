6949ddad53dc728f23a51c428a29caf0ba72da96
Kar Hoo Yiu <superk888@gmail.com>
2015-04-26 18:06:18 +0200

    Refactor ExpenseControllerTest into integration test + adding support of DBUnit

--- nxpense/src/test/java/controller/ExpenseControllerIntegrationTest.java
+++ nxpense/src/test/java/controller/ExpenseControllerIntegrationTest.java
@@ -8,12 +8,23 @@ import nxpense.dto.ExpenseResponseDTO;
 import nxpense.dto.ExpenseSource;
 import nxpense.dto.PageDTO;
 import nxpense.security.CustomUserDetails;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.joda.time.LocalDate;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -22,6 +33,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -30,7 +42,11 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,8 +58,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(locations = "classpath:/spring/application-test-config.xml")
 @WebAppConfiguration
 @Transactional(readOnly = true)
-// todo rename to integration test + include dataset for initialization data such as User
-public class ExpenseControllerTest {
+public class ExpenseControllerIntegrationTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExpenseControllerIntegrationTest.class);
 
     private static final String USER_EMAIL = "test@test.com";
     private static final String USER_PASSWORD = "secret";
@@ -57,11 +74,12 @@ public class ExpenseControllerTest {
     private static final Integer PAGE = 1;
     private static final Integer PAGE_SIZE = 25;
     private static final Sort.Direction SORT_DIRECTION = Sort.Direction.DESC;
-    private static final String [] SORT_PROPS = {"amount"};
-    private static final String EXPENSE_ID_1 = "1";
-    private static final String EXPENSE_ID_2 = "2";
+    private static final String[] SORT_PROPS = {"amount"};
 
     private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private DataSource datasource;
 
     @Autowired
     private WebApplicationContext wac;
@@ -70,8 +88,36 @@ public class ExpenseControllerTest {
 
     private MockMvc mockMvc;
 
-    private void mockAuthenticatedUser() {
+    private IDatabaseConnection getDBConnection() throws SQLException, DatabaseUnitException {
+        IDatabaseConnection iConnection = new DatabaseConnection(java.sql.DriverManager.getConnection("jdbc:h2:mem:nxpense;DB_CLOSE_DELAY=-1;user=sa;password=sa"));
+        return iConnection;
+    }
+
+    private IDataSet loadDataSet(String pathToDataset) throws DataSetException {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(pathToDataset);
+        IDataSet dataset = new FlatXmlDataSetBuilder().build(inputStream);
+        return dataset;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        DatabaseOperation.CLEAN_INSERT.execute(getDBConnection(), loadDataSet("dataset/expense-controller-integration-test-dataset.xml"));
+    }
+
+    private void mockAuthenticatedUser(int mockUserId) {
         User mockUser = new User();
+
+        try {
+            Field idField = User.class.getDeclaredField("id");
+            idField.setAccessible(true);
+            idField.set(mockUser, Integer.valueOf(mockUserId));
+            idField.setAccessible(false);
+        } catch (NoSuchFieldException e) {
+            LOGGER.error("Attempt to access a field that does not exist.", e);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("Attempt to access a non-accessible field.", e);
+        }
+
         mockUser.setEmail(USER_EMAIL);
         mockUser.setPassword(USER_PASSWORD.toCharArray());
 
@@ -82,7 +128,7 @@ public class ExpenseControllerTest {
 
     @Test
     public void testCreateExpense() throws Exception {
-        mockAuthenticatedUser();
+        mockAuthenticatedUser(1);
 
         mockMvcBuilder = MockMvcBuilders.webAppContextSetup(wac);
         mockMvcBuilder.alwaysExpect(status().isCreated());
@@ -134,6 +180,8 @@ public class ExpenseControllerTest {
 
     @Test
     public void testListPage() throws Exception {
+        mockAuthenticatedUser(1);
+
         mockMvcBuilder = MockMvcBuilders.webAppContextSetup(wac);
         mockMvcBuilder.alwaysExpect(status().isOk());
         mockMvc = mockMvcBuilder.build();
@@ -152,11 +200,12 @@ public class ExpenseControllerTest {
         assertThat(responsePage.getSortProperty()).isEqualTo(SORT_PROPS[0]);
         assertThat(responsePage.getPageNumber()).isEqualTo(PAGE);
         assertThat(responsePage.getSortDirection()).isEqualTo(SORT_DIRECTION);
+        assertThat(responsePage.getItems()).hasSize(3);
     }
 
     @Test
     public void testDelete() throws Exception {
-        mockAuthenticatedUser();
+        mockAuthenticatedUser(2);
 
         mockMvcBuilder = MockMvcBuilders.webAppContextSetup(wac);
         mockMvcBuilder.alwaysExpect(status().isOk());
@@ -164,9 +213,11 @@ public class ExpenseControllerTest {
 
         RequestBuilder requestBuilder = delete("/expense")
                 .contentType(MediaType.APPLICATION_JSON)
-                .param(PARAM_IDS, EXPENSE_ID_1)
-                .param(PARAM_IDS, EXPENSE_ID_2);
+                .param(PARAM_IDS, "6");
 
         mockMvc.perform(requestBuilder).andDo(print());
+
+        int numberOfExpenseUser2 = JdbcTestUtils.countRowsInTableWhere(new JdbcTemplate(datasource), "EXPENSE", "user_id = 2");
+        assertThat(numberOfExpenseUser2).isEqualTo(2);
     }
 }
