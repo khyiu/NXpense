c7e0aa045515cd1c7f8fe63f34fa145135e20fde
Kar Hoo Yiu <superk888@gmail.com>
2015-09-01 21:21:04 +0200

    Fix displaying number of attachments associated to expense item

--- nxpense/src/main/java/nxpense/dto/ExpenseResponseDTO.java
+++ nxpense/src/main/java/nxpense/dto/ExpenseResponseDTO.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- *
- */
 public class ExpenseResponseDTO extends ExpenseDTO {
 
     private Integer id;
@@ -20,15 +17,15 @@ public class ExpenseResponseDTO extends ExpenseDTO {
         this.id = id;
     }
 
-    public void setNbAttachment(Integer nbAttachment) {
-        System.out.println("nbAttachment = " + nbAttachment);
-    }
-
     public void setAttachments(List<AttachmentResponseDTO> attachments) {
         this.attachments = attachments;
     }
 
     public List<AttachmentResponseDTO> getAttachments() {
         return Collections.unmodifiableList(attachments);
+    }
+
+    public Integer getNbAttachment() {
+        return attachments.size();
     }
 }
