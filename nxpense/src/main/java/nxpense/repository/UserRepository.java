960f03e4c2c3ba8868e3a6a68ef6fe63e64477d8
khyiu <superk888@gmail.com>
2015-09-30 15:28:33 +0200

    Fix Sonar issue

--- nxpense/src/main/java/nxpense/repository/UserRepository.java
+++ nxpense/src/main/java/nxpense/repository/UserRepository.java
@@ -3,7 +3,7 @@ package nxpense.repository;
 import nxpense.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-	
-	public User findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public User findByEmail(String email);
 }
