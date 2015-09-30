8a2dd514a46e864b59b61350ded15b67b77d0e88
khyiu <superk888@gmail.com>
2015-09-30 15:32:09 +0200

    Fix Sonar issue

--- nxpense/src/main/java/nxpense/security/CustomAuthenticationSuccessHandler.java
+++ nxpense/src/main/java/nxpense/security/CustomAuthenticationSuccessHandler.java
@@ -15,26 +15,26 @@ import java.io.PrintWriter;
  * A CustomAuthenticationSuccessHandler handles successful login requests by sending back a response whose content holds
  * the URL that the client is to follow. This mechanism serves as a work-around for clients that don't implicitly
  * follow redirection.
- * 
+ *
  * @author kar.hoo.yiu
  */
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(CustomAuthenticationSuccessHandler.class); 
 
-	public void onAuthenticationSuccess(HttpServletRequest successfulRequest, 
-			HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-		LOGGER.info("User [{}] logged in with success", authentication.getName());
-		
-		StringBuilder redirection = new StringBuilder()
-			.append(successfulRequest.getContextPath())
-			.append("/view/home.html");
-		
-		String encodedRedirection = response.encodeRedirectURL(redirection.toString());
-		
-		PrintWriter writer = response.getWriter().append(encodedRedirection);
-		writer.flush();
-		writer.close();
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomAuthenticationSuccessHandler.class);
+
+    public void onAuthenticationSuccess(HttpServletRequest successfulRequest,
+                                        HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        LOGGER.info("User [{}] logged in with success", authentication.getName());
+
+        StringBuilder redirection = new StringBuilder()
+                .append(successfulRequest.getContextPath())
+                .append("/view/home.html");
+
+        String encodedRedirection = response.encodeRedirectURL(redirection.toString());
+
+        PrintWriter writer = response.getWriter().append(encodedRedirection);
+        writer.flush();
+        writer.close();
+    }
 
 }
