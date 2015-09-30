ed1b404dde5ba108f86785e8002a18e8514a388f
khyiu <superk888@gmail.com>
2015-09-30 15:20:22 +0200

    Fix Sonar issue

--- nxpense/src/main/java/nxpense/controller/AccountController.java
+++ nxpense/src/main/java/nxpense/controller/AccountController.java
@@ -16,24 +16,24 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/account")
 public class AccountController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping(value = "/new", method = RequestMethod.POST)
-	@ResponseBody
-	public String createNewAccount(HttpServletRequest request, @RequestParam String email, @RequestParam char[] password, @RequestParam char[] passwordRepeat) {
-		// TODO add Spring validation
-		userService.createUser(email, password, passwordRepeat);
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    @ResponseBody
+    public String createNewAccount(HttpServletRequest request, @RequestParam String email, @RequestParam char[] password, @RequestParam char[] passwordRepeat) {
+        // TODO add Spring validation
+        userService.createUser(email, password, passwordRepeat);
 
-		StringBuilder redirection = new StringBuilder()
-			.append(request.getContextPath())
-			.append("/view/home.html");
-		
-		LOGGER.info("User [{}] has been created and is automatically logged in", email);
-		
-		return redirection.toString();
-	}
+        StringBuilder redirection = new StringBuilder()
+                .append(request.getContextPath())
+                .append("/view/home.html");
+
+        LOGGER.info("User [{}] has been created and is automatically logged in", email);
+
+        return redirection.toString();
+    }
 
 }
