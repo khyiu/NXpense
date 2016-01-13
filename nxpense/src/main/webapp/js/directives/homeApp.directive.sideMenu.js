73d2127967ae8eb275d34f5ed070f1d3a8ccfefa
khyiu <superk888@gmail.com>
2016-01-13 19:30:49 +0100

    Fixed menu entry toggling

--- nxpense/src/main/webapp/js/directives/homeApp.directive.sideMenu.js
+++ nxpense/src/main/webapp/js/directives/homeApp.directive.sideMenu.js
@@ -15,12 +15,38 @@
         };
 
         function SideMenuController() {
+            var self = this;
+
+            this.viewStates;
+
             this.logout = logout;
+            this.toggle = toggle;
+
+            initViewState();
 
             /////////////////////
 
+            function initViewState() {
+                self.viewStates = {
+                    expenses: 'collapsed',
+                    availableTags: 'collapsed',
+                    tags: 'collapsed'
+                }
+            }
+
             function logout() {
                 window.location.assign('/' + $rootScope.WEB_CONTEXT + '/logout');
+            }
+
+            function toggle($event, menuEntry) {
+                var state = self.viewStates[menuEntry];
+
+                if(!state) {
+                    throw new Error('View state for menu entry named [' + menuEntry + '] does not exist!');
+                }
+
+                self.viewStates[menuEntry] = (state === 'collapsed' ? 'open' : 'collapsed');
+                $event.preventDefault();
             }
         }
     }
