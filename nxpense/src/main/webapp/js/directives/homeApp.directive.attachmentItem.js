0f3690a105b6dc58758dd17bd622be35785d33d9
khyiu <superk888@gmail.com>
2016-01-28 10:34:06 +0100

    Extracted attachmentItem directive into dedicated file

---
+++ nxpense/src/main/webapp/js/directives/homeApp.directive.attachmentItem.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    angular.module('homeApp.directive').directive('nxAttachmentItem', attachmentItem);
+
+    function attachmentItem() {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: 'modal/attachment-item.html',
+            controller: function ($scope) {
+
+                $scope.removeAttachment = function (attachmentToRemove, $event) {
+                    var idxToDelete;
+
+                    if (attachmentToRemove && (_.isUndefined($event.keyCode) || $event.keyCode === 13)) {
+                        if (attachmentToRemove.hasOwnProperty('tempRandomId')) {
+                            idxToDelete = $scope.newAttachments.map(function (attachment) {
+                                return attachment.tempRandomId;
+                            }).indexOf(attachmentToRemove.tempRandomId);
+
+                            $scope.newAttachments.splice(idxToDelete, 1);
+                            $scope.newAttachmentsToDisplay.splice(idxToDelete, 1);
+                        } else {
+                            idxToDelete = $scope.attachmentsToDisplay.map(function (attachment) {
+                                return attachment.filename;
+                            }).indexOf(attachmentToRemove.filename);
+
+                            $scope.attachmentsToDisplay.splice(idxToDelete, 1);
+
+                            idxToDelete = $scope.expense.attachments.map(function (attachment) {
+                                return attachment.filename;
+                            }).indexOf(attachmentToRemove.filename);
+
+                            $scope.expense.attachments.splice(idxToDelete, 1);
+                        }
+                    }
+                };
+            }
+        };
+    }
+})();
